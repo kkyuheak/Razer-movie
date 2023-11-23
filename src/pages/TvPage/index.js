c42ea77674ec815af6c906ae458f7ec9c117950a
kkyuheak <gyuheak@gmail.com>
2023-11-23 13:40:13 +0900

    tvPage 업데이트중

--- src/pages/TvPage/index.js
+++ src/pages/TvPage/index.js
@@ -8,10 +8,12 @@ import "swiper/css/pagination";
 import "./TvPage.css";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const TvPage = () => {
   const [tvList, setTvList] = useState([]);
   const [topRatedTvList, setTopRatedTvList] = useState([]);
+  const [moreCount, setMoreCount] = useState(1);
 
   const navigate = useNavigate();
 
@@ -19,6 +21,10 @@ const TvPage = () => {
     getTvList();
     getTopRatedTvList();
   }, []);
+
+  useEffect(() => {
+    getMoreTvList();
+  }, [moreCount]);
 
   const getTvList = async () => {
     const response = await instance.get(`trending/tv/week`);
@@ -29,6 +35,14 @@ const TvPage = () => {
     const res = await instance.get("tv/top_rated");
     console.log(res);
     setTopRatedTvList(res.data.results);
+  };
+
+  const getMoreTvList = async () => {
+    const res = await instance.get(`tv/top_rated?page=${moreCount}`);
+    console.log(res);
+    const a = [...topRatedTvList, ...res.data.results];
+    setTopRatedTvList(a);
+    console.log(topRatedTvList);
   };
 
   return (
@@ -65,23 +79,29 @@ const TvPage = () => {
 
       <h1 className="tv-title">평점 높은 TV프로그램</h1>
 
-      <div>
-        <TvBanner>
-          {topRatedTvList.map((tvList) => {
-            return (
-              <li key={tvList.id}>
-                <TopTvImg
-                  src={`https://image.tmdb.org/t/p/original${tvList.poster_path}`}
-                  alt={tvList.name}
-                  onClick={() => {
-                    navigate(`/tvprogram/${tvList.id}`);
-                  }}
-                />
-              </li>
-            );
-          })}
-        </TvBanner>
-      </div>
+      <TvBanner>
+        {topRatedTvList.map((tvList) => {
+          return (
+            <TvItem key={tvList.id}>
+              <TopTvImg
+                src={`https://image.tmdb.org/t/p/original${tvList.poster_path}`}
+                alt={tvList.name}
+                onClick={() => {
+                  navigate(`/tvprogram/${tvList.id}`);
+                }}
+              />
+              <p className="tv-list-title">{tvList.name}</p>
+            </TvItem>
+          );
+        })}
+      </TvBanner>
+      <MoreBtn
+        onClick={() => {
+          setMoreCount(moreCount + 1);
+        }}
+      >
+        더 보기
+      </MoreBtn>
     </Container>
   );
 };
@@ -102,19 +122,41 @@ const Wrapper = styled.div`
 `;
 
 const TvBanner = styled.ul`
+  max-width: 830px;
   list-style: none;
   display: flex;
-  gap: 10px;
-  overflow-y: hidden;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin: 0 auto;
+`;
+
+const TvItem = styled.li`
+  width: 150px;
+  box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
 `;
 
 const TopTvImg = styled.img`
-  width: 155px;
-  height: 225px;
+  width: 150px;
+  height: 200px;
   display: block;
   cursor: pointer;
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
 
   &:hover {
     border: 2px solid rgb(19, 204, 6);
   }
+`;
+
+const MoreBtn = styled.button`
+  width: 250px;
+  height: 40px;
+  background-color: rgb(19, 204, 6);
+  border-radius: 10px;
+  color: #000;
+  border: none;
+  cursor: pointer;
+  display: block;
+  margin: 20px auto;
 `;
